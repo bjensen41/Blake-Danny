bef870cae6750890ef074945378568736fdb7e64
bjensen41 <jen13045@byui.edu>
2017-05-12 09:25:28 -0700

    Actor Class

---
+++ Actors.java
@@ -0,0 +1,27 @@
+
+
+package actor;
+
+
+import java.io.Serializable {
+/**
+ *
+ * @author blakejensen
+ */
+public class Actor implements Serializable{
+    private  String name;
+    private String actorName;
+    
+    public Actor () {
+    }
+    
+    public String getActorName() {
+       
+        return ActorName;
+    }
+    
+   public void setActorName (String actorName) {
+       this.actorName = actorName;
+   }
+}
+}
